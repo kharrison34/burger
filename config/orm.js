c94cc0e2c74da993da69f1b27191babbb772d8cd
kharrison34 <kyle.harrison344@gmail.com>
2018-07-22 18:59:23 -0400

    UP TO CONTROLLERS

---
+++ config/orm.js
@@ -0,0 +1,124 @@
+//import mysql connection
+
+var connection = require("./connection.js");
+
+function printQuestionMarks(num) {
+    var array = [];
+
+    for (var i = 0; i < num; i++) {
+        array.push("?");
+    }
+
+    return array.toString();
+}
+
+
+
+function objToSql(ob) {
+    var arr = [];
+
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+        var value = ob[key];
+        
+        if (Object.hasOwnProperty.call(ob, key)) {
+            
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+          
+            arr.push(key + "=" + value);
+        }
+    }
+
+  
+    return arr.toString();
+}
+
+
+
+var orm = {
+    all: function(table, cb) {
+        var queryString = "SELECT * FROM " + table;
+        
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    allOrder: function(table, orderCol, cb) {
+        var queryString = "SELECT * FROM " + table + " ORDER BY " + orderCol;
+        
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    allOrderDesc: function(table, orderCol, cb) {
+        var queryString = "SELECT * FROM " + table + " ORDER BY " + orderCol + " DESC";
+        
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    create: function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+  
+    update: function(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+    delete: function(table, condition, cb) {
+        var queryString = "DELETE FROM " + table;
+
+        queryString += " WHERE ";
+        queryString += condition;
+
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    }
+};
+
+module.exports = orm;
---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express')();
+var bodyParser = require('body-parser');
+
+var mysql = require('mysql')
+
+
+//set up the express app
+
+var app = express();
+var port = process.env.port || 8080;
+
+//require models used for syncing 
+var db = require("./models");
+
+//sets up the express app to handle data parsing
+
+// parse application 
+app.use(bodyParser.urlencoded({ extended: true}));
+//parse application and json
+app.use(bodyParser.json());
+
+
+//static directory
+app.use(express.static("public"));
+
+//routes
+
+require("./routes/html-routes.js")(app);
+
+//syncing the sequilize models and then starting the exress app
+
+db.sequelize.sync({ force: true }).then(function() {
+    app.listen(PORT, function() {
+      console.log("App listening on PORT " + PORT);
+    });
+});
